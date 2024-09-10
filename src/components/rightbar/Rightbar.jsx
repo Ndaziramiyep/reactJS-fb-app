d5dff3472e2e056c1f9e3dcd44a2ee12bcbf3c9b
Ndaziramiyep <patrickofficial30@gmail.com>
2024-09-10 18:58:11 +0200

    using state and props in react project

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from "../sidebar/Sidebar"
+import "./rightbar.css"
+export default function Rightbar(){
+    let [name,setName]=useState("");
+    let [email,setEmail]=useState("");
+    let [count,setCount]=useState(0);
+      function clickCounter(){
+        count++; -m 
+        setCount(count);
+        console.log(count);
+      }
+    const displayValues=(e)=>{
+         e.preventDefault();
+       const demo=document.getElementById("demo");
+       if(name.length > 0){
+        demo.innerHTML=`Hello ${name}, your Email is: ${email}.`;
+        }
+        else{
+        demo.innerHTML=`invalid input!`;
+        }
+      }
 
-const Rightbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return(
+        <div className="rightbar">
+        {/*    <div className="messangers share">
+        //         <p>Patrick</p>
+        //     </div>}
+ <> {/*          <Sidebar/>  */}
+             <form action='' onSubmit={displayValues}>
+         <label>Name:</label>   <input type="text" onChange={(e)=>{setName(name=e.target.value)}} /><br/>
+         <label>Email:</label>   <input type="text" onChange={(e)=>{setEmail(email=e.target.value)}} /><br/>
+            <button type='submit'>Submit</button>
+            </form> 
+            <button onClick={clickCounter}>counter {count}</button>
+            <h1 id='demo'></h1>
+        </div>
+    
+
+    )
 }
-
-export default Rightbar
